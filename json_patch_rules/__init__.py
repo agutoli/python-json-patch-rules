3c9e21ef2fac17469fcb71e7fdaf237b92c81d71
Bruno Agutoli <bruno.agutoli@rotorstudios.com>
2024-05-04 14:03:20 +1000

    wip - big refactoring

--- json_patch_rules/__init__.py
+++ json_patch_rules/__init__.py
@@ -1,228 +1,73 @@
 import re
-import json
-import pydash
 from typing import Any, Optional, Pattern
 from dataclasses import dataclass
-from json_patch_rules.__symbols__ import EMPTY_ARRAY_SYMBOL
-
+import pydash
 
 @dataclass
 class RuleItem:
-    rule_pattern: str
-    replace: bool = False
-    unique: bool = False
-    replace_path: Optional[str] = None
-    pattern: Optional[str] = None
-    compile: Optional[Pattern[str]] = None
-
-@dataclass
-class PatchResult:
-    denied_paths: list[str]
-    successed_paths: list[str]
-    is_patched: bool
-    patched_data: Any
-    errors: list[str]
-
-
-@dataclass
-class PatchingStore:
-    old_data: Any = None
-    new_data: Any = None
-
+    rule: str
+    path: str
+    actions: set
+    deny: bool = False
+    pattern: Optional[Pattern[str]] = None
 
 class JsonPatchRules:
-    ANY_INDEX_WILDCARD     = "0__ANY_INDEX_WILDCARD__0"
-    UNIQUE_ITEMS_LIST  = "0__UNIQUE_ITEMS_LIST__0"
-    REPLACE_ITEMS_LIST = "0__REPLACE_ITEMS_LIST__0"
-    ANY_KEY_WILDCARD   = "0__ANY_KEY_WILDCARD__0"
-    ANY_SEG_WILDCARD   = "0__ANY_SEG_WILDCARD__0"
+    TOKEN_KEY_REPLACE = '{*}|replace'
+    TOKEN_ROOT_REPLACE = '*|replace'
+
+    # Constants for regex patterns
+    PATTERN_WILDCARD_ANY = r'.*'
+    PATTERN_WILDCARD_ANY_KEY = r'[^.]+(?:\.[^.]+)*'
+    PATTERN_WILDCARD_ANY_INDEX = r'\[\d+\]'
 
     def __init__(self, rules) -> None:
-        self.rules = rules
+        self.rules = [self.parse_rule(rule) for rule in rules]
 
-    def get_paths(self, obj, current_path=""):
-        """ Recursively find all paths in a nested JSON object and format them in dot and bracket notation. """
-        if isinstance(obj, dict):
-            for k, v in obj.items():
-                new_path = f"{current_path}.{k}" if current_path else k
-                yield from self.get_paths(v, new_path)
-        elif isinstance(obj, list):
-            for i, v in enumerate(obj):
-                new_path = f"{current_path}[{i}]"
-                yield from self.get_paths(v, new_path)
-        else:
-            yield current_path
-
-    def parse_rule(self, rule):
-        rule_item = RuleItem(rule_pattern=rule)
-
-        """Convert a permission rule into a regex pattern for matching paths."""
-        # Temporary placeholders for complex wildcards to avoid premature escaping
-        placeholder_dict = {
-            '[:unique:]': self.UNIQUE_ITEMS_LIST,
-            '[:replace:]': self.REPLACE_ITEMS_LIST,
-            '[*]': self.ANY_INDEX_WILDCARD,
-            '{*}': self.ANY_KEY_WILDCARD,
-            '*': self.ANY_SEG_WILDCARD,
-        }
-
-        for key, value in placeholder_dict.items():
-            rule = rule.replace(key, value)
-
-        # Escape the rule to safely turn into regex, then restore placeholders to regex patterns
-        rule = re.escape(rule)
-        rule = rule.replace(re.escape(self.ANY_INDEX_WILDCARD), r'\[\d+\]')
-        rule = rule.replace(re.escape(self.ANY_KEY_WILDCARD), r'[^.]+')
-        rule = rule.replace(re.escape(self.ANY_SEG_WILDCARD), r'.+')
-
-        # Replace {key1,key2,...} with regex alternation group
-        def replace_options(match):
-            options = match.group(1)
-            options = '|'.join(re.escape(option.strip()) for option in options.split(','))
-            return f"(?:{options})"
-
-        rule = re.sub(r'\\{([^\\}]+)\\}', replace_options, rule)
-
-        # Check if using :unique: and remove all rules after that
-        # given we can replace the whole array doing a regular variable assign
-        # instead of using pydash.set_()
-        unique_items_parts = rule.split(self.UNIQUE_ITEMS_LIST)
-        if len(unique_items_parts) > 1:
-            rule = unique_items_parts[0] # ignore all properties after [:unique:]
-            rule_item.replace = True
-            rule_item.unique = True
-            rule_item.replace_path = unique_items_parts[0]
-
-        # Allow replace the whole array
-        replace_items_parts = rule.split(self.REPLACE_ITEMS_LIST)
-        if len(replace_items_parts) > 1:
-            rule = rule.replace(re.escape(self.REPLACE_ITEMS_LIST), r'.*')
-            rule_item.replace = True
-            rule_item.unique = False
-            rule_item.replace_path = replace_items_parts[0]
+    def parse_rule(self, rule: str) -> RuleItem:
+        deny = rule.startswith('!')
+        new_rule = rule[1:] if deny else rule
+        parts = new_rule.split('|')
         
-        rule_item.compile = re.compile('^' + rule + '$')
-        return rule_item
+        path = parts[0]
+        path = path.replace('*', self.PATTERN_WILDCARD_ANY)
+        path = path.replace('{*}', self.PATTERN_WILDCARD_ANY_KEY)
+        path = path.replace('[*]', self.PATTERN_WILDCARD_ANY_INDEX)
 
-    def replace_with_value(self, data_to_patch, path, values) -> None:
-            pydash.set_(data_to_patch, path, values)
+        actions = set(parts[1:]) if len(parts) > 1 else {'replace', 'set'}
+        pattern = re.compile(f'^{path}$')
+        return RuleItem(rule=rule, path=path, actions=actions, deny=deny, pattern=pattern)
 
-    def patch_value(self, data_to_patch, path, values) -> None:
-        if not values:
-            pydash.unset(data_to_patch, path)
-        else:
-            pydash.set_(data_to_patch, path, values)
+    def verify_permission(self, data_path: str, action: str, new_value: Any) -> bool:
+        for rule in self.rules:
+            # It is trying to replace the root value with a new object
+            if self.TOKEN_KEY_REPLACE == rule.rule:
+                if isinstance(new_value, dict):
+                    return True
 
-    def verify_permission(self, data_paths, permission_rules):
-        """Check if the provided data paths are allowed by the permission rules."""
-        rule_items = [self.parse_rule(rule) for rule in permission_rules]
-        results = {}
-        for path in data_paths:
-            for rule_item in rule_items:
-                if rule_item.replace and rule_item.replace_path:
-                    results[rule_item.replace_path] = [True, rule_item]
-                elif rule_item.compile:
-                    if rule_item.replace:
-                        results[path] = [True, rule_item]
-                    else:
-                        results[path] = [rule_item.compile.match(path), rule_item]
-                else:
-                    results[path] = [False, None]
-        return results
-
-    def item_checker(self, path, allowed, store, successed, denied_paths, errors):
-        if not allowed[0]:
-            denied_paths.append(path)
-            return successed, denied_paths, errors
-        
-        if isinstance(store.new_data, list):
-            # print(path, EMPTY_ARRAY_SYMBOL, store.new_data)
-            # and store.new_data[0] == EMPTY_ARRAY_SYMBOL
-            self.patch_value(store.old_data, path, None)
-            successed.append(path)
-            return successed, denied_paths, errors
-
-        if allowed[1].replace and not allowed[1].unique:
-            self.replace_with_value(store.old_data, path, pydash.get(store.new_data, path))
-            successed.append(path)
-            return successed, denied_paths, errors
-
-        if not allowed[1].unique:
-            self.patch_value(store.old_data, path, pydash.get(store.new_data, path))
-            successed.append(path)
-            return successed, denied_paths, errors
-
-        items_values = pydash.get(store.new_data, path)
-        if isinstance(items_values, list):
-            try:
-                # It can not have boolean
-                bools_in_list = [x for x in items_values if isinstance(x, bool)]
-                if len(bools_in_list) > 0:
-                    error_message = (
-                        f"Failed to make unique: boolean found in list at '{path}'. "
-                        f"Boleans cannot be used with the [:unique:] modifier because they are unhashable. "
-                        f"Encountered error while processing rule: {allowed[1].rule_pattern}."
-                    )
-                    errors.append([path, error_message])
-                    denied_paths.append(path)
-                    return successed, denied_paths, errors
-                items_values = list(dict.fromkeys(items_values))
-            except TypeError as err:
-                if "unhashable type: 'dict'" in str(err):
-                    error_message = (
-                        f"Failed to make unique: dictionaries found in list at '{path}'. "
-                        f"Dictionaries and other mutable types cannot be used with the [:unique:] modifier because they are unhashable. "
-                        f"Encountered error while processing rule: {allowed[1].rule_pattern}."
-                    )
-                    errors.append([path, error_message])
-                    denied_paths.append(path)
-                    return successed, denied_paths, errors
-                
-        self.patch_value(store.old_data, path, items_values)
-        successed.append(path)
-        return successed, denied_paths, errors
+        return any(
+            rule.pattern.match(data_path) and action in rule.actions and not rule.deny
+            for rule in self.rules
+        )
 
     def apply(self, old_data, new_data):
-        # First validation is check if same type, otherwise raise error
-        if isinstance(old_data, list) != isinstance(new_data, list):
-            old_data_type = type(old_data).__name__
-            new_data_type = type(new_data).__name__
-            raise TypeError(f"The data and the patch must have the same types. old_data={old_data_type}, new_data={new_data_type}")
+        return self.merge_changes(old_data, new_data)
 
-        store = PatchingStore(
-            old_data=pydash.clone_deep(old_data),
-            new_data=pydash.clone_deep(new_data),
-        )
+    def apply_patch(self, data, path, value, action):
+        if self.verify_permission(path, action, value):
+            pydash.set_(data, path, value)
+        else:
+            print(f"Permission denied for action '{action}' on path '{path}'.")
 
-        # Fix when need to setup empty values
-        if not store.new_data and isinstance(store.new_data, list):
-            store.new_data = [EMPTY_ARRAY_SYMBOL]
-
-        results = self.verify_permission(list(self.get_paths(store.new_data)), self.rules)
-
-        errors = []
-        successed = []
-        denied_paths = []
-
-        for path, allowed in results.items():
-            successed, denied_paths, errors = self.item_checker(
-                path,
-                allowed,
-                store,
-                successed,
-                denied_paths,
-                errors
-            )
-
-        return PatchResult(
-            errors=errors,
-            patched_data=store.old_data,
-            is_patched=len(denied_paths) == 0,
-            denied_paths=denied_paths,
-            successed_paths=successed
-        )
-
+    def merge_changes(self, old_data, new_data, path=''):
+        for key, new_value in new_data.items():
+            current_path = f"{path}.{key}" if path else key
+            if self.verify_permission(current_path, 'replace', new_value):
+                pydash.set_(old_data, current_path, new_value)
+            else:
+                old_value = pydash.get(old_data, current_path)
+                action = 'replace' if old_value is not None else 'set'
+                self.apply_patch(old_data, current_path, new_value, action)
+        return old_data
 
 def patch_rules(rules):
-    jsonpatch = JsonPatchRules(rules)
-    return jsonpatch
+    return JsonPatchRules(rules)
--- tests/test_module.py
+++ tests/test_module.py
@@ -1,164 +1,62 @@
+# test_json_patch_rules.py
+
 import pytest
-from json_patch_rules import patch_rules
+from json_patch_rules import JsonPatchRules, patch_rules
 
-@pytest.fixture
-def sample_data():
-    return {
-        "root_key_1": {
-            "dog_key_1": False,
-            "dog_key_2": 123,
-            "dog_key_3": "bark"
-        },
-        "root_key_2": {
-            "cat_key_1": False,
-            "cat_key_2": [
-                "cat_array_1",
-                "cat_array_2"
-            ]
-        },
-        "root_key_3": ["root_array_1", "root_array_2"]
-    }
+def test_replace_at_root_level():
+    rules = ["*|replace"]
+    patch = patch_rules(rules)
+    old_data = {"user": {"name": "old"}}
+    new_data = {"user": {"name": "new"}}
+    result = patch.apply(old_data, new_data)
+    assert result["user"]["name"] == "new", "Should replace the entire user object"
 
-def test_denied_paths(sample_data):
-    patch = patch_rules([
-        "root_key_1.dog_key_1"  # Only this path is allowed
-    ])
+def test_deny_replace_at_root_level():
+    rules = ["!*|replace"]
+    patch = patch_rules(rules)
+    old_data = {"user": {"name": "old"}}
+    new_data = {"user": {"name": "new"}}
+    result = patch.apply(old_data, new_data)
+    assert result["user"]["name"] == "old", "Should not replace the entire user object due to deny rule"
 
-    result = patch.apply(sample_data, {
-        "root_key_1": {
-            "dog_key_2": "it will be denied"
-        }
-    })
+def test_replace_any_object_keys_values():
+    rules = ["{*}|replace"]
+    patch = patch_rules(rules)
+    old_data = {"user": {"name": "old", "age": 30}}
+    new_data = {"user": {"name": "new", "age": 30}}
+    result = patch.apply(old_data, new_data)
+    assert result["user"]["name"] == "new", "Should replace name key in any object"
 
-    assert result.denied_paths == ["root_key_1.dog_key_2"], "Should deny updates to non-whitelisted paths"
+def test_replace_any_list_index_values():
+    rules = ["[*]|replace"]
+    patch = patch_rules(rules)
+    old_data = [{"user": {"name": "old", "age": 30}}]
+    new_data = [{"user": {"name": "new", "age": 30}}]
+    result = patch.apply(old_data, new_data)
+    assert result[0]["user"]["name"] == "new", "Should replace name key in any object"
 
+def test_deny_replace_any_object_keys_values():
+    rules = ["!{*}|replace"]
+    patch = patch_rules(rules)
+    old_data = {"user": {"name": "old", "age": 30}}
+    new_data = {"user": {"name": "new", "age": 30}}
+    result = patch.apply(old_data, new_data)
+    assert result["user"]["name"] == "old", "Should deny replacing name key in any object"
 
-def test_raise_exception_when_diff_types(sample_data):
-    patch = patch_rules([
-        "*"
-    ])
+def test_implicit_set_allowed():
+    rules = ["user"]
+    patch = patch_rules(rules)
+    old_data = {"user": {"name": "old"}}
+    new_data = {"user": {"age": 30}}  # Attempt to add a new key
+    result = patch.apply(old_data, new_data)
+    assert "age" in result["user"], "Should allow setting new attributes"
 
-    with pytest.raises(TypeError, match="The data and the patch must have the same types. old_data=list, new_data=dict"):
-        patch.apply([], {})
+def test_implicit_set_denied():
+    rules = ["!user"]
+    patch = patch_rules(rules)
+    old_data = {"user": {"name": "old"}}
+    new_data = {"user": {"age": 30}}  # Attempt to add a new key
+    result = patch.apply(old_data, new_data)
+    assert "age" not in result["user"], "Should deny setting new attributes"
 
-
-def test_wildcard_rules_succeed_paths(sample_data):
-    patch = patch_rules(["root_key_1.*"])  # All sub-keys of root_key_1 are allowed
-
-    result = patch.apply(sample_data, {
-        "root_key_1": {
-            "dog_key_1": True,
-            "dog_key_2": 456  # Changed from 1 to 456 for clarity
-        }
-    })
-
-    assert result.successed_paths == ['root_key_1.dog_key_1', 'root_key_1.dog_key_2'], \
-        "Should allow updates to all fields under root_key_1 due to wildcard rule"
-
-
-def test_enum_keys_rules_succeed_paths(sample_data):
-    patch = patch_rules(["root_key_1.{dog_key_1,dog_key_3}"])  # Allow specific keys under root_key_1
-
-    result = patch.apply(sample_data, {
-        "root_key_1": {
-            "dog_key_1": True,
-            "dog_key_2": "this will be denied",  # Expect this to be denied
-            "dog_key_3": "new bark"
-        }
-    })
-
-    assert result.successed_paths == ['root_key_1.dog_key_1', 'root_key_1.dog_key_3'], \
-        "Should only allow updates to dog_key_1 and dog_key_3"
-    assert "root_key_1.dog_key_2" in result.denied_paths, "dog_key_2 update should be denied"
-
-def test_allow_replace_array_with_wildcard(sample_data):
-    patch = patch_rules(["root_key_3[*]"])  # Allow all items in the array at root_key_3
-
-    result = patch.apply(sample_data, {
-        "root_key_3": ["new_array_1", "new_array_2", "new_array_3"]
-    })
-
-    assert result.successed_paths == ['root_key_3[0]', 'root_key_3[1]', 'root_key_3[2]'], \
-        "Should allow updates to all elements in the root_key_3 array"
-
-def test_allow_replace_array_unique_values(sample_data):
-    patch = patch_rules(["root_key_3[:unique:]"])
-
-    result = patch.apply(sample_data, {
-        "root_key_3": ["a", "b", "b", "b", 1, 2, 2]
-    })
-
-    assert result.patched_data["root_key_3"] == ['a', 'b', 1, 2], \
-        "Should remove duplicated values"
-
-
-def test_allow_replace_array(sample_data):
-    patch = patch_rules(["root_key_3[:replace:]"])
-
-    result = patch.apply(sample_data, {
-        "root_key_3": ["replaced_value"]
-    })
-
-    assert result.patched_data["root_key_3"] == ["replaced_value"], \
-        "Should replace the whole array with anything"
-
-def test_overwrite_array_with_empty(sample_data):
-    patch = patch_rules([
-        "*"
-    ])
-
-    result = patch.apply([sample_data], [])
-    assert result.patched_data == [], \
-        "Should replace the root level of the array with empty list"
-
-def test_empty_values_dict(sample_data):
-    patch = patch_rules([])
-    result = patch.apply({}, {})
-    assert result.patched_data == {}, \
-        "Should return same types"
-
-def test_empty_values_list(sample_data):
-    patch = patch_rules([])
-    result = patch.apply([], [])
-    assert result.patched_data == [], \
-        "Should return same types"
-
-
-def test_empty_values_list(sample_data):
-    patch = patch_rules([
-        "level_1:replace.*"
-    ])
-    result = patch.apply({
-        "level_1": {
-            "level_2": "abc"
-        }
-    }, {})
-
-    assert result.patched_data == {}, \
-        "Should return same types"
-
-
-# def test_set_new_property_in_object_within_array(sample_data):
-#     patch = patch_rules([
-#         "[*].foo"
-#     ])
-
-#     result = patch.apply([sample_data], [
-#         {"foo": "bar"}
-#     ])
-
-#     assert result.patched_data == [{**sample_data, "foo": "bar"}], \
-#         "Should replace the root level of the array with empty list"
-
-
-# def test_set_new_property_in_object_within_array(sample_data):
-#     patch = patch_rules([
-#         "[:replace:]"
-#     ])
-
-#     result = patch.apply([sample_data], [
-#         {"foo": "bar"}
-#     ])
-
-#     assert result.patched_data == [{**sample_data, "foo": "bar"}], \
-#         "Should replace the root level of the array with empty list"
+# Continue writing tests for each rule spec
